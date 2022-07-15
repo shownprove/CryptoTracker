0a95ed38a63df8ecf259d073331d0b6ab3980be5
oddcode <oddcode@naver.com>
2022-07-16 06:03:40 +0900

    challenge 제출

--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,7 @@ import { DefaultTheme } from "styled-components";
 
 export const dark: DefaultTheme = {
   bgColor: "#2f3640",
-  textColor: "#f5f6fa",
+  textColor: "#282828",
   accentColor: "#9c88ff",
 };
 
